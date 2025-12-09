c0915c6296ec26dea55205b915b4c3595a6b57e3
Yash <136283288+Yashkutwal1307@users.noreply.github.com>
2025-12-09 16:27:49 +0530

    Create if_else.py

---
+++ 02_control_flow/if_else.py
@@ -0,0 +1,16 @@
+#Age eligibility
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
+
+# Even or Odd
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
