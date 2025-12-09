3440521330bf213842754af0399c9f67e0ffd17b
Yash <136283288+Yashkutwal1307@users.noreply.github.com>
2025-12-09 16:28:40 +0530

    Create loops.py

---
+++ 02_control_flow/loops.py
@@ -0,0 +1,15 @@
+# For loop example
+
+print("For loop example:")
+
+for i in range(1, 6):
+    print("Number:", i)
+
+# While loop example
+
+print("\nWhile loop example:")
+
+count = 1
+while count <= 5:
+    print("Count:", count)
+    count += 1
