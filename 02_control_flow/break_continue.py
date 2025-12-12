46f6356a513519f80fef37477ddf4521e56ca3d5
Yash <136283288+Yashkutwal1307@users.noreply.github.com>
2025-12-12 23:26:45 +0530

    Create break_continue.py

---
+++ 02_control_flow/break_continue.py
@@ -0,0 +1,13 @@
+print("Break example:")
+
+for i in range(1, 10):
+    if i == 5:
+        break
+    print(i)
+
+print("\nContinue example:")
+
+for i in range(1, 10):
+    if i == 5:
+        continue
+    print(i)
